ef35d83831456b115a6a134d91483255662e8011
Chris Prather <chris@prather.org>
2009-10-30 16:27:41 -0400

    stop being stupid

--- lib/Moses/Declare/Syntax/EventKeyword.pm
+++ lib/Moses/Declare/Syntax/EventKeyword.pm
@@ -1,20 +1,12 @@
 use MooseX::Declare;
 
 class Moses::Declare::Syntax::EventKeyword extends
-  MooseX::Declare::Syntax::Keyword::MethodModifier {
-    use Moose::Util::TypeConstraints;
-
-    has modifier_type => (
-        is       => 'rw',
-        isa      => 'Str',
-        required => 1,
-    );
+  MooseX::Declare::Syntax::Keyword::Method {
 
     sub register_method_declaration {
         my ( $self, $meta, $name, $method ) = @_;
         $meta->add_state_method( $name => $method );
     }
-
 }
 
 1;
